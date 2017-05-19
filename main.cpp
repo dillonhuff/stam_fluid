dd12230cd4faceb4f0ee2156c0c146f3b4a9367f
Dillon Huff <dillonhuff@gmail.com>
2017-05-18 22:37:58 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#define IX(i, j) ((i) + (N+2)*(j))
+
+int main() {
+  int N = 5;
+  int size = (N+2)*(N+2);
+  int u[size], v[size], u_prev[size], v_prev[size];
+  int dens[size], dens_prev[size];
+  
+  
+  return 0;
+}
