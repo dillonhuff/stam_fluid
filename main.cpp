502f98c695da638162315fdb906a81b71303682e
Dillon Huff <dillonhuff@gmail.com>
2017-05-24 16:13:14 -0700

    Something realistic looking is now happening with diffusion, velocity still looks broken

--- main.cpp
+++ main.cpp
@@ -229,8 +229,10 @@ int main() {
 
 	dens_prev[IX(i, j)] = 255.0; //255.0; //random_float(0, 255);
 	dens[IX(i, j)] = 255.0; //random_float(0, 255);
+
 	u_prev[IX(i, j)] = 0.0; //cube_vel_x;//random_float(0, 1);
 	v_prev[IX(i, j)] = 0.0; //cube_vel_y;//-1*u_prev[i];//random_float(0, 1);
+
 	u[IX(i, j)] = cube_vel_x;//random_float(0, 1);
 	v[IX(i, j)] = cube_vel_y;//random_float(0, 1);
 
