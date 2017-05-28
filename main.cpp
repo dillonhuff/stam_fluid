1216ace94a43130a20fde3ce1492e667839defa6
Dillon Huff <dillonhuff@gmail.com>
2017-05-28 00:48:08 -0700

    Breaking up into multiple test cases

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
 #include "fluid.h"
 #include "utils.h"
 
@@ -5,7 +9,86 @@
 
 using namespace std;
 
-int main() {
+TEST_CASE("One step on 20 by 20 grid, constant velocity") {
+  int N = 200;
+  int size = (N+2)*(N+2);
+
+  float u[size], v[size], u_prev[size], v_prev[size];
+  double dt = 0.1;
+  double visc = 0.00;
+  double diff = 100.00;
+
+  int cube_bl_x = 50;
+  int cube_bl_y = 150;
+  int cube_length = 30;
+
+  float cube_vel_x = 10.0;
+  float cube_vel_y = 0.0;
+
+  float dens[size], dens_prev[size];
+
+  for (int i = 1; i <= N; i++) {
+    for (int j = 1; j <= N; j++) {
+
+      u_prev[IX(i, j)] = cube_vel_x;
+      v_prev[IX(i, j)] = 0.0;
+
+      u[IX(i, j)] = cube_vel_x;
+      v[IX(i, j)] = 0.0;
+      
+      if ((cube_bl_x <= i && i <= cube_bl_x + cube_length) &&
+	  (cube_bl_y <= j && j <= cube_bl_y + cube_length)) {
+
+	dens_prev[IX(i, j)] = 255.0;
+	dens[IX(i, j)] = 255.0;
+
+      } else {
+
+	dens_prev[IX(i, j)] = 0;
+	dens[IX(i, j)] = 0;
+
+      }
+    }
+
+  }
+
+  set_bnd(N, 0, dens_prev);
+  set_bnd(N, 1, u_prev);
+  set_bnd(N, 2, v_prev);
+  
+  set_bnd(N, 0, dens);
+  set_bnd(N, 1, u);
+  set_bnd(N, 2, v);
+
+  cout << "Initialized density" << endl;
+  //visualize_density(N, dens);
+
+  cout << "Initialized u" << endl;
+  //visualize_density(N, u);
+
+  cout << "Initialized v" << endl;
+  //visualize_density(N, v);
+  
+  vel_step ( N, u, v, u_prev, v_prev, visc, dt );
+
+  // visualize_density(N, u);
+  // visualize_density(N, v);
+
+  cout << "Did vel step" << endl;
+
+  dens_step ( N, dens, dens_prev, u, v, diff, dt );
+
+  //read_values( N, dens_prev, u_prev, v_prev );
+
+  cout << "Did dens step" << endl;
+
+  //visualize_density(N, dens);
+
+  //visualize_density(N, dens);
+
+}
+
+TEST_CASE("Full simulation 200 by 200 grid") {
   int N = 200;
   int size = (N+2)*(N+2);
 
@@ -106,6 +189,4 @@ int main() {
   //visualize_density(N, dens);
 
   
-
-  return 0;
 }
